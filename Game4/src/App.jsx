81d777cf2abdc191d8a511188ca8398be0a4015b
SALAH-NAME <phone4ouahidi@gmail.com>
2024-01-06 10:30:35 +0100

    first_commit

---
+++ Game4/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import Game from './Game';
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <h1>Game Web</h1>
+        <Game />
+    </>
+  )
+}
+
+export default App
---
+++ Game4/src/Card.jsx
@@ -0,0 +1,18 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Card() {
+
+	return (
+		<>
+			<div>
+				<img src={viteLogo} className="logo" alt="Vite logo" />
+			</div>
+			<div>
+				<h2>Vite</h2>
+			</div>
+		</>
+	)
+}
+
+export default Card
